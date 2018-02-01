ad93d284f5afb23a6bb41814722afe80d3911f91
satheesh kannan <smarteye9626@gmail.com>
2018-02-01 14:19:37 +0530

    Create even.java

---
+++ even.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	Scanner ob=new Scanner(System.in);
+	int x=ob.nextInt();
+	if(x/2==0)
+	 System.out.println(x+" "+"Even");
+	 else 
+	 x=x-1;
+	 System.out.println(x+" "+"Even");
+	}
+}
