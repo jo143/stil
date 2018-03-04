b3058a5ed180920792fb8b15a1426a47a733d9a2
satheesh kannan <smarteye9626@gmail.com>
2018-03-04 21:30:04 +0530

    Create  Ideone.java

---
+++ Ideone.java
@@ -0,0 +1,23 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args)
+	{
+	Scanner ob=new Scanner(System.in);
+	int k=ob.nextInt();
+	if(k%7==0)
+	{
+		System.out.print("yes");
+	}
+	else
+	{
+		System.out.print("no");
+	}
+	}
+}
